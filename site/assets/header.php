5dcd69bd775b73372ad10e2a61ecf4fd758d60f4
Casey <casey.cp@att.net>
2014-09-13 17:11:24 -0400

    Small styling change to the header + added link to homepage

--- site/assets/header.php
+++ site/assets/header.php
@@ -1,7 +1,6 @@
 <div class='container header'>
 	<div class="col-md-4">
-		<p>Please report any bugs/glitches to me@ameobea.me</p>
-		<p>Feedback is also appreciated!</p>
+	<h1><a href="http://ameobea.me/gawtrack/">GAWTrack</a></h1>
 	</div>
 	<div class="col-md-4">
 		<h3>GAW Events History Analysis Tool</h3>
@@ -9,5 +8,6 @@
 	</div>
 	<div class="col-md-4">
 		<p>GAWTrack version 0.1</p>
+		<p>Please report any bugs/glitches to me@ameobea.me<br>Feedback is also appreciated!</p>
 	</div>
 </div>
